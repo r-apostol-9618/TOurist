dd71394dd91a8bed4924d9758ea25082c1cd1eb9
r.apostol <r.apostol.9618@gmail.com>
2019-10-04 16:22:45 +0200

    Initial cry

---
+++ app/src/main/java/com/its/tourist/BudgetFragment.java
@@ -0,0 +1,30 @@
+package com.its.tourist;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BudgetFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_budget, container, false);
+    }
+    public BudgetFragment(){
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+    }
+}
--- app/src/main/java/com/its/tourist/MainActivity.java
+++ app/src/main/java/com/its/tourist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.its.tourist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler mHandler = new Handler();
+            //Aspetto 5 secondi e passo alla nuova activity
+            mHandler.postDelayed(new Runnable() {
+                public void run() {
+                    BudgetFragment bf = new BudgetFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_main,bf);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            }, 5000);
+        }
     }
-}
+
+
+
